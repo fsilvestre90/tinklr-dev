dc5f71c50b44279ea0148cfb75cdfe23b6d8ae25
Jeff Austin <jeffaustin81@gmail.com>
2015-08-31 15:21:54 -0700

    Add delete, and getAll to Marker class

--- src/Marker.php
+++ src/Marker.php
@@ -98,6 +98,31 @@
 			$this->id = $GLOBALS['DB']->lastInsertId();
         }
 		
+		function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM markers WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM bathrooms_markers WHERE marker_id = {$this->getId()};");
+        }
+		
+		static function getAll()
+        {
+            $returned_markers = $GLOBALS['DB']->query("SELECT * from markers;");
+            
+            $markers = array();
+            
+            foreach($returned_markers as $marker) {
+                $name = $marker['name'];
+				$address = $marker['address'];
+				$city = $marker['city'];
+				$state = $marker['state'];
+				$zipcode = $marker['zipcode'];
+				$type = $marker['type'];
+                $id = $marker['id'];
+                $new_marker = new Marker($name, $address, $city, $state, $zipcode, $type, $id);
+                array_push($markers, $new_marker);
+            }
+            return $markers;
+        }
 		
 	}	
 ?>
