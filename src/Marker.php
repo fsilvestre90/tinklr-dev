fc862959b512b1f0f1f99cb958ad685ebf55454c
Jeff Austin <jeffaustin81@gmail.com>
2015-08-31 14:52:41 -0700

    Add getters, setters for Marker class

--- src/Marker.php
+++ src/Marker.php
@@ -19,5 +19,72 @@
 			$this->type = $type;
 			$this->id = $id;
 		}
+		
+		function getName()
+		{
+			return $this->name;
+		}
+		
+		function setName($new_name)
+		{
+			$this->name = $new_name;
+		}
+		
+		function getAddress()
+		{
+			return $this->address;
+		}
+		
+		function setAddress($new_address)
+		{
+			$this->address = $new_address;
+		}
+		
+		function getCity()
+		{
+			return $this->city;
+		}
+		
+		function setCity($new_city)
+		{
+			$this->city = $new_city;
+		}
+		
+		function getState()
+		{
+			return $this->state;
+		}
+		
+		function setState($new_state)
+		{
+			$this->state = $new_state;
+		}
+		
+		function getZipcode()
+		{
+			return $this->zipcode;
+		}
+		
+		function setZipcode($new_zipcode)
+		{
+			$this->zipcode = $new_zipcaddress;	
+		}
+		
+		function getType()
+		{
+			return $this->type;
+		}
+		
+		function setType($new_type)
+		{
+			$this->type = $new_type;
+		}
+		
+		function getId()
+		{
+			return $this->id;
+		}
+		
+		
 	}	
 ?>
