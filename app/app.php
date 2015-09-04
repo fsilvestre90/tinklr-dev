c6c88ae5f54cfcbd3c03e952f4d7f447cb506d5b
Filipe Silvestre <fssilvestre5@gmail.com>
2015-09-03 17:08:43 -0700

    finalize review

--- app/app.php
+++ app/app.php
@@ -143,10 +143,12 @@
         $app->post('/add_review/{id}', function($id) use ($app){
             $review = $_POST['review'];
             $rating = $_POST['rating'];
+            $new_review = new Review($rating, $review);
+            $new_review->save();
 
-            var_dump($review);
-            $bathroom = Bathroom::find($id);
             $marker = Marker::find($id);
+            $bathroom = Bathroom::find($marker->getId());
+            var_dump($bathroom);
             $reviews = Review::getReviewsForBathroom($bathroom);
 
             return $app['twig']->render('bathroom.html.twig', array('bathroom' => $bathroom, 'marker' => $marker, 'reviews' => $reviews));
